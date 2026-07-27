343b269cba06ae8322f9a6cbeb5a5af6e18fdf5e
Saba Siddiqui <ms.sabasiddiqui2004@gmail.com>
2026-07-27 17:46:59 +0000

    done

--- supabase/functions/generate-quiz/index.ts
+++ supabase/functions/generate-quiz/index.ts
@@ -16,15 +16,12 @@ Deno.serve(async (req) => {
     const OPENROUTER_KEY = Deno.env.get("OPENROUTER_API_KEY1");
 
     const MODELS = [
-      // "google/gemma-3-4b-it:free",
-      // "mistralai/mistral-7b-instruct:free",
-      // "meta-llama/llama-3.2-3b-instruct:free",
-      // "qwen/qwen-2.5-7b-instruct:free",
-     "google/gemma-3-12b-it:free",
-     "google/gemma-3-4b-it:free", 
-     "meta-llama/llama-3.1-8b-instruct:free",
-     "mistralai/mistral-7b-instruct:free",
-    "deepseek/deepseek-r1-0528:free",
+      s
+    //  "google/gemma-3-12b-it:free",
+    //  "google/gemma-3-4b-it:free", 
+    //  "meta-llama/llama-3.1-8b-instruct:free",
+    //  "mistralai/mistral-7b-instruct:free",
+    // "deepseek/deepseek-r1-0528:free",
     ];
 
     const prompt = `Generate exactly 10 multiple choice questions from this text.
